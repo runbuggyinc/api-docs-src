aa6fcb7ec2f12d2bca0bfe52ff4e24f5f6f4ba2e
eric.cumberworth@runbuggy.com <eric.cumberworth@runbuggy.com>
2023-07-17 10:29:42 -0700

    Modified 3 files
    Committed by RunBuggyAdmin on behalf of eric.cumberworth@runbuggy.com

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMDc5MQ/spectral.js?branch_slug=eric-cumberworth-modified-3-files&token=c501f34e-d1de-4514-8920-08359a470d99";
+export default { extends: ruleset };
